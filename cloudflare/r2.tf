4810a76f3d84771428cb438b2ab9c13fb541aff5
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-11-18 11:51:07 +0000

    r2 bucket for ui

--- cloudflare/r2.tf
+++ cloudflare/r2.tf
@@ -3,3 +3,9 @@ resource "aws_s3_bucket" "tails_r2_bucket" {
 
   bucket = "tail-database-tails-${each.value}"
 }
+
+resource "aws_s3_bucket" "ui_r2_bucket" {
+  for_each = { for environment in var.environments : environment => environment }
+
+  bucket = "tail-database-ui-${each.value}"
+}
